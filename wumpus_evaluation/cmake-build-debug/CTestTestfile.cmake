68438d1b0f65a170ba9d5271aa6f35a8eb25de55
Lisa Martmann <l.martmann@gmx.de>
2019-12-11 15:19:16 +0100

    remove cmake build debug

--- wumpus_evaluation/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lisa/wumpusws/src/cnc-wumpus/wumpus_evaluation
-# Build directory: /home/lisa/wumpusws/src/cnc-wumpus/wumpus_evaluation/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
